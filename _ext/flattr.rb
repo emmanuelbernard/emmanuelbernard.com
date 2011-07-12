a42d0214d7e9af85e08dd07c4315d780f98e6de6
Emmanuel Bernard <emmanuel@hibernate.org>
2011-07-13 01:45:39 +0200

    Make extension nicer

--- _ext/flattr.rb
+++ _ext/flattr.rb
@@ -1,6 +1,4 @@
 
-require 'digest/sha1'
-
 module Awestruct
   module Extensions
     class Flattr
@@ -15,36 +13,30 @@ module Awestruct
           html += %Q|var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];|
           html += %Q|s.type = 'text/javascript';\n|
           html += %Q|s.async = true;\n|
-          html += %Q|s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto&uid=#{site.flattr_userid}&category=text';\n|
+          html += %Q|s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto&uid=#{site.flattr_username}&category=text';\n|
           html += %Q|t.parentNode.insertBefore(s, t);\n|
           html += %Q|})(); /* ]]> */ </script>|
           html
         end
         def flattr_large_counter(param={})
           url = param[:url] ? param[:url] : site.base_url + self.url
-          title = param[:title] ? param[:title] : site.title
+          title = param[:title] ? param[:title] : self.title
           category = param[:category] ? param[:category] : "text"
           tags = param[:tags] ? "tags:" + param[:tags] + ";" : ""
           html = %Q|<a class="FlattrButton" style="display:none;" href="#{url}" title="#{title}" |
-          html += %Q|rev="flattr;uid:#{site.flattr_userid};category:#{category};#{tags}"></a>|
-          # rev="flattr;category:text;tags:tag,tag2,tag3;"
-          # html += %Q|<noscript><a href="http://flattr.com/thing/342256/Emmanuel-Bernard-website" target="_blank">|
-          # html += %Q|<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>|
+          html += %Q|rev="flattr;uid:#{site.flattr_username};category:#{category};#{tags}"></a>|
           html
         end
-        def flattr_compact_counter()
-          html = %Q|<a class="FlattrButton" style="display:none;" rev="flattr;button:compact;" href="http://emmanuelbernard.com"></a>|
-          html += %Q|<noscript><a href="http://flattr.com/thing/342256/Emmanuel-Bernard-website" target="_blank">|
-          html += %Q|<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a></noscript>|
-          html
-        end
-        def flattr_static()
-          html = %Q|<a href="http://flattr.com/thing/342256/Emmanuel-Bernard-website" target="_blank">|
-          html += %Q|<img src="http://api.flattr.com/button/flattr-badge-large.png" alt="Flattr this" title="Flattr this" border="0" /></a>|
+        def flattr_compact_counter(param={})
+          url = param[:url] ? param[:url] : site.base_url + self.url
+          title = param[:title] ? param[:title] : self.title
+          category = param[:category] ? param[:category] : "text"
+          tags = param[:tags] ? "tags:" + param[:tags] + ";" : ""
+          html = %Q|<a class="FlattrButton" style="display:none;" href="#{url}" title="#{title}" |
+          html += %Q|rev="flattr;button:compact;uid:#{site.flattr_username};category:#{category};#{tags}"></a>|
           html
         end
       end
     end
   end
-
 end
