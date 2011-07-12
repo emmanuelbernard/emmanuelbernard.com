ca56be717de52bc7cbdea1542377be48cbf61c86
Emmanuel Bernard <emmanuel@hibernate.org>
2011-07-12 22:58:25 +0200

    Activate Flattr button on the side and activate Flattr extension

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,9 @@
+require 'flattr'
 
 Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new( '/news' ) 
   extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Flattr.new
   helper Awestruct::Extensions::Partial
   helper Awestruct::Extensions::GoogleAnalytics
 end
