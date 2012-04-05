3dddf6a5a2fdb9410730b57b23c34f30c527d392
Emmanuel Bernard <emmanuel@hibernate.org>
2012-04-05 23:25:19 +0200

    Add Blog + atom feed + tags + flattr per blog entry

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,7 +1,11 @@
 Awestruct::Extensions::Pipeline.new do
   # extension Awestruct::Extensions::Posts.new( '/news' ) 
-  extension Awestruct::Extensions::Indexifier.new
+  extension Awestruct::Extensions::Posts.new('/blog', :posts)
+  extension Awestruct::Extensions::Paginator.new( :posts, '/blog/index', :per_page=>10 )
+  extension Awestruct::Extensions::Tagger.new( :posts, '/blog/index', '/blog/tags', :per_page=>10 )
+  extension Awestruct::Extensions::Atomizer.new( :posts, '/blog/blog.atom', :num_entries=>10000 )
   extension Awestruct::Extensions::Flattr.new
+  extension Awestruct::Extensions::Indexifier.new
   helper Awestruct::Extensions::Partial
   helper Awestruct::Extensions::GoogleAnalytics
 end
