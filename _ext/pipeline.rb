a2010531488cf0dba602bb0e7dbaff5ec6d2da7b
Emmanuel Bernard <emmanuel@hibernate.org>
2012-04-11 15:08:06 +0200

    Add TagClouds to side bar and add dedicated tab page

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -6,6 +6,7 @@ Awestruct::Extensions::Pipeline.new do
   extension Awestruct::Extensions::Posts.new('/blog', :posts)
   extension Awestruct::Extensions::Paginator.new( :posts, '/blog/index', :per_page=>10 )
   extension Awestruct::Extensions::Tagger.new( :posts, '/blog/index', '/blog/tags', :per_page=>10, :sanitize=>true )
+  extension Awestruct::Extensions::TagCloud.new(:posts, '/blog/tags/index.html', :layout=>'tab', :title=>'Tags')
   extension Awestruct::Extensions::Indexifier.new
   extension Awestruct::Extensions::Atomizer.new( 
     :posts, 
